d0de690305be754f0b21f94c16daf6c10c2aad5c
Ravi kumar <Ravi kumar@DESKTOP-26DU0CT>
2025-11-22 15:21:37 +0530

    Dev added ArithmeticOperation.java and Main.java

---
+++ GITProj01/src/main/java/com/ravi/main/Main.java
@@ -0,0 +1,11 @@
+package com.ravi.main;
+
+import com.ravi.service.ArithmeticOperation;
+
+public class Main {
+	public static void main(String[] args) {
+	ArithmeticOperation ao=new ArithmeticOperation();
+	
+	System.out.println("Result: "+ao.add(10, 20));
+}
+}
---
+++ GITProj01/src/main/java/com/ravi/service/ArithmeticOperation.java
@@ -0,0 +1,8 @@
+package com.ravi.service;
+
+public class ArithmeticOperation {
+
+	public int add(int a,int b) {
+		return a+b;
+	}
+}
